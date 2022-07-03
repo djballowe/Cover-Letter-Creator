39f259bc5f4d516383fe622dfd95ea8dbe9916c3
djballowe <djballowe@gmail.com>
2022-07-03 16:08:35 -0700

    about section works its a miracle

--- cover-letter-app/src/Components/About.js
+++ cover-letter-app/src/Components/About.js
@@ -5,35 +5,58 @@ class About extends Component {
   constructor() {
     super();
 
+    this.handleClick = this.handleClick.bind(this);
+
     this.state = {
       about: "",
+      class: "about-form",
     };
   }
 
+  onSubmitAbout = (e) => {
+    e.preventDefault();
+    this.setState({
+      class: "about-form",
+    });
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      about: e.target.value,
+    });
+  };
+
+  handleClick = (e) => {
+    this.setState({
+      class: "about-form-display",
+    });
+  };
+
   render() {
+    const text = this.state;
     return (
       <div className="about">
         <div className="about-text">
           <h1>About</h1>
         </div>
         <div className="text-body">
-          <p>
-            Pellentesque habitant morbi tristique senectus et netus et malesuada
-            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
-            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
-            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
-            eleifend leo.
-          </p>
+          <p>{text.about}</p>
           <div className="icon-container">
-            <img src={edit} alt="" />
+            <button
+              className="edit-button"
+              onClick={this.handleClick}
+              type="submit"
+            >
+              <img src={edit} alt="" />
+            </button>
           </div>
-          <form className="about-form" onSubmit={this.handleSubmit}>
+          <form className={text.class} onSubmit={this.onSubmitAbout}>
             <textarea
               name="about"
               id=""
               cols="30"
               rows="10"
-              onChange={this.handleState}
+              onChange={this.handleChange}
             ></textarea>
             <button>Done</button>
           </form>

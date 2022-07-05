ac585108c7c41434c20324199e52905da1bafb71
djballowe <djballowe@gmail.com>
2022-07-05 11:04:07 -0700

    changing the class components to functional components with hooks

--- cover-letter-app/src/Components/About.js
+++ cover-letter-app/src/Components/About.js
@@ -1,70 +1,41 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import edit from "../Images/pencil.svg";
 
-class About extends Component {
-  constructor() {
-    super();
+function About() {
+  const [isActive, setActive] = useState("false");
 
-    this.handleClick = this.handleClick.bind(this);
-
-    this.state = {
-      about:
-        "This is a sample of the About me page. Please edit this and fill out your own information below.",
-      class: "hide",
-    };
+  const ToggleClass = () => {
+    setActive(!isActive);
   }
 
-  onSubmitAbout = (e) => {
-    e.preventDefault();
-    this.setState({
-      class: "hide",
-    });
-  };
-
-  handleChange = (e) => {
-    this.setState({
-      about: e.target.value,
-    });
-  };
-
-  handleClick = (e) => {
-    this.setState({
-      class: "about-form-display",
-    });
-  };
-
-  render() {
-    const text = this.state;
-    return (
-      <div className="about">
-        <div className="about-text">
-          <h1>About</h1>
-        </div>
-        <div className="text-body">
-          <p>{text.about}</p>
-          <div className="icon-container">
-            <button
-              className="edit-button"
-              onClick={this.handleClick}
-              type="submit"
-            >
-              <img src={edit} alt="" />
-            </button>
-          </div>
-          <form className={text.class} onSubmit={this.onSubmitAbout}>
-            <textarea
-              name="about"
-              id=""
-              cols="30"
-              rows="10"
-              onChange={this.handleChange}
-            ></textarea>
-            <button>Done</button>
-          </form>
-        </div>
+  return (
+    <div className="about">
+      <div className="about-text">
+        <h1>About</h1>
       </div>
-    );
-  }
+      <div className="text-body">
+        <p></p>
+        <div className="icon-container">
+          <button
+            className="edit-button"
+            onClick={ToggleClass}
+            type="submit"
+          >
+            <img src={edit} alt="" />
+          </button>
+        </div>
+        <form className={isActive ? 'hide' : "about-form-display"}>
+          <textarea
+            name="about"
+            id=""
+            cols="30"
+            rows="10"
+          ></textarea>
+          <button>Done</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default About;

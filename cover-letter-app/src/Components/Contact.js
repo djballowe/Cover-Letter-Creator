1ce196d23c7fd7f2cabd40fdefb1d3a398d68ea6
djballowe <djballowe@gmail.com>
2022-07-05 18:32:36 -0700

    contact changed

--- cover-letter-app/src/Components/Contact.js
+++ cover-letter-app/src/Components/Contact.js
@@ -1,106 +1,97 @@
-import React, { Component } from "react";
+import React, { useState } from "react";
 import edit from "../Images/pencil.svg";
 
-class Contact extends Component {
-  constructor() {
-    super();
+function Contact() {
+  // constructor() {
+  //   super();
 
-    this.handleClick = this.handleClick.bind(this);
+  //   this.handleClick = this.handleClick.bind(this);
 
-    this.state = {
-      phone: "555-555-5555",
-      email: "example@example.com",
-      website: "examplewebsite/example.com",
-      linkedin: "DonSmith/linkedin",
-      class: "hide",
-    };
-  }
+  //   this.state = {
+  //     phone: "555-555-5555",
+  //     email: "example@example.com",
+  //     website: "examplewebsite/example.com",
+  //     linkedin: "DonSmith/linkedin",
+  //     class: "hide",
+  //   };
+  // }
 
-  onSubmitContact = (e) => {
+  const [isActive, setActive] = useState("false");
+  const [Phone, setPhone] = useState("555-555-5555");
+  const [Email, setEmail] = useState("example@example.com");
+  const [Website, setWebsite] = useState("examplewebsite/example.com");
+  const [Linkedin, setLinkedin] = useState("DonSmith/linkedin");
+
+  const ToggleClass = () => {
+    setActive(!isActive);
+  };
+
+  const submit = (e) => {
     e.preventDefault();
-    this.setState({
-      class: "hide",
-    });
+    ToggleClass();
   };
 
-  handleChange = (e) => {
-    if (e.target.name === "phone") {
-      this.setState({
-        phone: e.target.value,
-      });
-    } else if (e.target.name === "email") {
-      this.setState({
-        email: e.target.value,
-      });
-    } else if (e.target.name === "website") {
-      this.setState({
-        website: e.target.value,
-      });
-    } else if (e.target.name === "linkedin") {
-      this.setState({
-        linkedin: e.target.value,
-      });
+  const handleChange = (e) => {
+    if (e.target.name === 'phone') {
+      setPhone(e.target.value)
+    } else if (e.target.name === 'email') {
+      setEmail(e.target.value)
+    } else if(e.target.name === 'website') {
+      setWebsite(e.target.value) 
+    } else if(e.target.name === 'linkedin') {
+      setLinkedin(e.target.value)
     }
-  };
-
-  handleClick = (e) => {
-    this.setState({
-      class: "contact-form-display",
-    });
-  };
-
-  render() {
-    const info = this.state;
-    return (
-      <div className="about">
-        <div className="about-text">
-          <h1>Contact</h1>
-        </div>
-        <div className="contact-text-body">
-          <p>Phone: {info.phone}</p>
-          <p>Email: {info.email}</p>
-          <p>Portfolio: {info.website}</p>
-          <p>Linkedin: {info.linkedin}</p>
-          <div className="icon-container">
-            <button
-              className="edit-button"
-              onClick={this.handleClick}
-              type="submit"
-            >
-              <img src={edit} alt="" />
-            </button>
-          </div>
-          <form className={info.class} onSubmit={this.onSubmitContact}>
-            <input
-              type="text"
-              placeholder="Phone"
-              onChange={this.handleChange}
-              name="phone"
-            />
-            <input
-              type="text"
-              placeholder="Email"
-              onChange={this.handleChange}
-              name="email"
-            />
-            <input
-              type="text"
-              placeholder="Website"
-              onChange={this.handleChange}
-              name="website"
-            />
-            <input
-              type="text"
-              placeholder="Linkedin"
-              onChange={this.handleChange}
-              name="linkedin"
-            />
-            <button>Done</button>
-          </form>
-        </div>
-      </div>
-    );
   }
+
+  return (
+    <div className="about">
+      <div className="about-text">
+        <h1>Contact</h1>
+      </div>
+      <div className="contact-text-body">
+        <p>Phone: {Phone}</p>
+        <p>Email: {Email}</p>
+        <p>Portfolio: {Website}</p>
+        <p>Linkedin: {Linkedin}</p>
+        <div className="icon-container">
+          <button
+            className="edit-button"
+            onClick={ToggleClass}
+            type="submit"
+          >
+            <img src={edit} alt="" />
+          </button>
+        </div>
+        <form className={isActive ? "hide" : "about-form-display"} onSubmit={submit}>
+          <input
+            type="text"
+            placeholder="Phone"
+            onChange={handleChange}
+            name="phone"
+          />
+          <input
+            type="text"
+            placeholder="Email"
+            onChange={handleChange}
+            name="email"
+          />
+          <input
+            type="text"
+            placeholder="Website"
+            onChange={handleChange}
+            name="website"
+          />
+          <input
+            type="text"
+            placeholder="Linkedin"
+            onChange={handleChange}
+            name="linkedin"
+          />
+          <button>Done</button>
+        </form>
+      </div>
+    </div>
+  );
 }
 
 export default Contact;

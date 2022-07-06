3c29d0c35257fdc1dcd66bbf6d0941e9be4467ce
djballowe <djballowe@gmail.com>
2022-07-05 17:37:00 -0700

    about changed to use hooks

--- cover-letter-app/src/Components/About.js
+++ cover-letter-app/src/Components/About.js
@@ -3,9 +3,19 @@ import edit from "../Images/pencil.svg";
 
 function About() {
   const [isActive, setActive] = useState("false");
+  const [about, setAbout] = useState('');
 
   const ToggleClass = () => {
     setActive(!isActive);
+  }
+
+  const submit = e => {
+    e.preventDefault()
+    ToggleClass();
+  }
+
+  const handleChange = e => {
+    setAbout(e.target.value)
   }
 
   return (
@@ -14,7 +24,7 @@ function About() {
         <h1>About</h1>
       </div>
       <div className="text-body">
-        <p></p>
+        <p>{about}</p>
         <div className="icon-container">
           <button
             className="edit-button"
@@ -24,12 +34,13 @@ function About() {
             <img src={edit} alt="" />
           </button>
         </div>
-        <form className={isActive ? 'hide' : "about-form-display"}>
+        <form className={isActive ? 'hide' : "about-form-display"} onSubmit={submit}>
           <textarea
             name="about"
             id=""
             cols="30"
             rows="10"
+            onChange={handleChange}
           ></textarea>
           <button>Done</button>
         </form>
